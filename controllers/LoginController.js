761d07f6c97a0285cfc0406af83ff58b61f4bbb3
Roberta Neukamp Guerreiro <betaneuk@hotmail.com>
2022-08-19 10:37:26 -0300

    Mudanças

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -8,99 +8,57 @@ const LoginController = {
     },
 
     auth: (req, res) => {
-        const errors = validationResult(req);
+    //VALIDAÇÃO LOGIN //
+        loginAuth: (req, res) => {
+        res.clearCookie('user');
+        res.clearCookie('admin');
 
-        if(!errors.isEmpty()) {
-            return res.status(400).json({
-                success: false,
-                errors: errors.array()
-            });
-        }
+        const usersJson = fs.readFileSync(
+            path.join(__dirname, "..", "data", "users.json"),
+            "utf-8"
+        );
 
-        res.status(200).json({
-            success: true,
-            message: 'Login sucessful'
+        const users = JSON.parse(usersJson);
+
+        const { email, password } = req.body;
+        const userAuth = users.find((user) => {
+            if (user.email === email) {
+                if (bcrypt.compareSync(password, user.password)){
+                    return true;
+                }
+                // O if de cima é a mesma coisa da linha abaixo
+        // return bcrypt.compareHash(senha, user.senha);
+            }
         });
 
-        return res.redirect('store');
-
-    },
-    
-    // auth (req, res) {
-    //     const loginReceived = req.body;
-    //     const errors = [];
-
-    //     const loginFound = Register.find(registerReceived.email);
-
-    //     console.log(registerFound);
-    //     if(!registerFound) {
-    //         errors.push({ msg: 'Login inválido' });
-    //         return res.render('store', { errors, register: registerrReceived });
-    //     }
-
-    //     const samePassword = registerr.comparePassword(loginReceived.password, loginFound.re_password);
-
-    //     if (!samePassword) {
-    //         errors.push({ msg: 'Login inválido' });
-    //         return res.render('store', { errors, register: registerReceived });
-    //     }
-
-    //     req.session.register = registerReceived;
-
-    //     return res.redirect('store');
-    // },
-
-    // VALIDAÇÃO LOGIN //
-    //     loginAuth: (req, res) => {
-//         res.clearCookie('user');
-//         res.clearCookie('admin');
-
-//         const usersJson = fs.readFileSync(
-//             path.join(__dirname, "..", "data", "users.json"),
-//             "utf-8"
-//         );
-
-//         const users = JSON.parse(usersJson);
-
-//         const { email, senha } = req.body;
-//         const userAuth = users.find((user) => {
-//             if (user.email === email) {
-//                 if (bcrypt.compareSync(password, user.password)){
-//                     return true;
-//                 }
-//                 // O if de cima é a mesma coisa da linha abaixo
-//         // return bcrypt.compareHash(senha, user.senha);
-//             }
-//         });
-
-//         if (!userAuth) {
-//             return res.render('login', {
-//                 title: "Faça Login | Mijukim",
-//                 user: req.cookies.user,
-//                 old: req.body,
-//                 error: {
-//                     message: "Email or password required"
-//                 }
-//             });
-//         }  
-//             //Filtra as chaves que o objeto irá ter
-//         const user = json.parse(
-//         JSON.stringify(userAuth, ["id", "FullName", "password", "admin"])
-//         );
-//         req.session.email = userAuth.email;
-//         res.cookie("user", user);
-//         res.cookie("admin", user.admin);
+        if (!userAuth) {
+            return res.render('login', {
+                title: "Faça Login | Mijukim",
+                user: req.cookies.user,
+                old: req.body,
+                error: {
+                    message: "Email or password required"
+                }
+            });
+        }  
+            //Filtra as chaves que o objeto irá ter
+        const user = json.parse(
+        JSON.stringify(userAuth, ["id", "FullName", "password", "admin"])
+        );
+        req.session.email = userAuth.email;
+        res.cookie("user", user);
+        res.cookie("admin", user.admin);
         
-//         res.redirect("/");
-//     },
-//   // Processamento do deslogar
+        res.redirect("/profile");
+    }
+  // Processamento do deslogar
 //   logout: (req, res) => {
 //     req.session.destroy();
 //     res.clearCookie("user");
 //     res.clearCookie("admin");
 //     res.redirect("/");
 //     }
-// }
+}
 }
 
 module.exports = LoginController;
