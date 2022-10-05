695be0a4a8938b9edbf4824517d0cacff0685b96
Roberta <betaneuk@hotmail.com>
2022-10-05 12:07:04 -0300

    Modificações no cartController, botão delete no cart e ajustes na responsividade e css

--- src/controllers/RegisterController.js
+++ src/controllers/RegisterController.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { name } = require('ejs');
 const { validationResult } = require('express-validator');
-const { Register } = require('../models');
+const { Register, Cart } = require('../models');
 const bcrypt = require('../helpers/bcrypt');
 
 const RegisterController = {
@@ -24,24 +24,29 @@ const RegisterController = {
                 old: req.body,
             })
         } else {
-            const { FullName, email, password, country, adressLine, suburb, state, postcode, phone, emailOption } = req.body;
+            const { FullName, email, password, country, addressLine, suburb, state, postcode, phone, emailOption } = req.body;
 
             let newUser = {
-                FullName, 
+                fullName, 
                 email, 
                 password: bcrypt.generateHash(password), //cripto.. senha
                 country, 
-                adressLine, 
+                firstName,
+                lastName,
+                address,
+                address2, 
                 suburb, 
                 state,
                 postcode,
                 phone,
                 emailOption,
-                creadoEm: new Date(),
-                modificadoEm: new Date()
+                createdAt: new Date(),
+                updatedAt: new Date()
             };
 
-            await Register.create(newUser);
+            const user = await Register.create(newUser);
+
+            await Cart.create({ id_client: user.id_client });
 
             res.redirect('login')
         }
