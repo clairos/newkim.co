6af771c34f75f81c59fd4222659bb1c7bf8b6b32
Suane Oliveira <oliveiraanara25@gmail.com>
2022-10-11 00:16:03 -0300

    menu hamburguer funcionando, mas falta estilizar

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -20,19 +20,31 @@
 //     document.getElementById('menu-bg').classList.toggle('change-bg');
 // }
  //      MENU HAMBURGER
-let navigation = document.querySelector('.navigation');
-document.querySelector('.toggle').onclick = function (){
-    this.classList.toggle('active');
-    navigation.classList.toggle('active');
+//  let navigation = document.querySelector('.navigation');
+//  document.querySelector('.toggle').onclick = function (){
+//      this.classList.toggle('active');
+//      navigation.classList.toggle('active');
+//  }
+function onClickMenu(){
+    document.getElementById('menu').classList.toggle('change');
+    document.getElementById('nav').classList.toggle('change');
+    document.getElementById('menu-bg').classList.toggle('change');
 }
 //      SEARCH
-let search = document.querySelector('.search');
-let close = document.querySelector('.close');
-let searchBox = document.querySelector('.searchBox');
+// let search = document.querySelector('.search');
+// let close = document.querySelector('.close');
+// let searchBox = document.querySelector('.searchBox');
 
-search.onclick = function(){
-    searchBox.classList.add('active');
-}
-close.onclick = function(){
-    searchBox.classList.remove('active');
-}    
+// search.onclick = function(){
+//     searchBox.classList.add('active');
+// }
+// close.onclick = function(){
+//     searchBox.classList.remove('active');
+// }    
+const search = document.querySelector('.search')
+const btn = document.querySelector('.searchInput')
+const input = document.querySelector('.input')
+btn.addEventListener('click', () => {
+search.classList.toggle('active')
+input.focus()
+})
