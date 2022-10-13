785653a74bd7902e5030e6671f1fd48aa3f70c91
Suane Oliveira <oliveiraanara25@gmail.com>
2022-10-13 10:29:48 -0300

    resolvendo conflitos

---
+++ public/javascripts/menubar.js
@@ -0,0 +1,48 @@
+// const close = document.querySelector('.menu-search');
+// const open = document.querySelector('.ham');
+// const menu = document.querySelector('.menu');
+
+// close.addEventListener('click', () => {
+//     menu.style.visibility = 'hidden';
+// })
+
+// open.addEventListener('click', () => {
+//     menu.style.visibility = 'visible'
+// });
+
+// function openSearch(){
+//     this.style.display = 'none'
+// }
+
+
+ //      MENU HAMBURGER
+//  let navigation = document.querySelector('.navigation');
+//  document.querySelector('.toggle').onclick = function (){
+//      this.classList.toggle('active');
+//      navigation.classList.toggle('active');
+//  }
+function onClickMenu(){
+    document.getElementById('menu').classList.toggle('change');
+    document.getElementById('nav').classList.toggle('change');
+    document.getElementById('menu-bg').classList.toggle('change');
+}
+//      SEARCH
+// let search = document.querySelector('.search');
+// let close = document.querySelector('.close');
+// let searchBox = document.querySelector('.searchBox');
+
+// search.onclick = function(){
+//     searchBox.classList.add('active');
+// }
+// close.onclick = function(){
+//     searchBox.classList.remove('active');
+// }    
+const search = document.querySelector('.search')
+const btn = document.querySelector('.searchInput')
+const input = document.querySelector('.input')
+btn.addEventListener('click', () => {
+search.classList.toggle('active')
+input.focus()
+})
+
+// icon login
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-// const close = document.querySelector('.menu-search');
-// const open = document.querySelector('.ham');
-// const menu = document.querySelector('.menu');
-
-// close.addEventListener('click', () => {
-//     menu.style.visibility = 'hidden';
-// })
-
-// open.addEventListener('click', () => {
-//     menu.style.visibility = 'visible'
-// });
-
-// function openSearch(){
-//     this.style.display = 'none'
-// }
-
-// function onClickMenu(){
-//     document.getElementById('menu').classList.toggle('change');
-//     document.getElementById('nav').classList.toggle('change');
-//     document.getElementById('menu-bg').classList.toggle('change-bg');
-// }
- //      MENU HAMBURGER
-//  let navigation = document.querySelector('.navigation');
-//  document.querySelector('.toggle').onclick = function (){
-//      this.classList.toggle('active');
-//      navigation.classList.toggle('active');
-//  }
-function onClickMenu(){
-    document.getElementById('menu').classList.toggle('change');
-    document.getElementById('nav').classList.toggle('change');
-    document.getElementById('menu-bg').classList.toggle('change');
-}
-//      SEARCH
-// let search = document.querySelector('.search');
-// let close = document.querySelector('.close');
-// let searchBox = document.querySelector('.searchBox');
-
-// search.onclick = function(){
-//     searchBox.classList.add('active');
-// }
-// close.onclick = function(){
-//     searchBox.classList.remove('active');
-// }    
-const search = document.querySelector('.search')
-const btn = document.querySelector('.searchInput')
-const input = document.querySelector('.input')
-btn.addEventListener('click', () => {
-search.classList.toggle('active')
-input.focus()
-})
-=======
 function onChange(id_cart) {
   const form = document.getElementById('cartForm');
   form.addEventListener('onsubmit', function (e) {
@@ -68,5 +16,3 @@ function onChange(id_cart) {
   })
   return false;
 }
-
->>>>>>> 30725c50ad9622e5f64750815592f8cbb6b94624
